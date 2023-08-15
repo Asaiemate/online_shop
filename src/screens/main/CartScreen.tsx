74ec7dda53c77c4eee4a018130718bc02ed7e3bc
Konstantin <Asaiemate@gmail.com>
2023-08-15 12:50:53 +0900

    cart screen layout

--- src/screens/main/CartScreen.tsx
+++ src/screens/main/CartScreen.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {useAppSelector} from '../../redux/store';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ItemSeparatorComponent} from './ItemsList';
 
 export const CartScreen = () => {
-  return <Text>CartList</Text>;
+  const cart = useAppSelector(state => state.cart);
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={cart}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    borderWidth: 1,
+    borderRadius: 16,
+    borderColor: 'gray',
+    marginHorizontal: 16,
+  },
+});
--- src/screens/main/ItemsList.tsx
+++ src/screens/main/ItemsList.tsx
@@ -218,7 +218,7 @@ export const ItemsList = (props: Props) => {
     </SafeAreaView>
   );
 };
-const ItemSeparatorComponent = () => <View style={styles.separator} />;
+export const ItemSeparatorComponent = () => <View style={styles.separator} />;
 
 const styles = StyleSheet.create({
   container: {
